5780da63940696b1ff3c4de9f2b5cbba0d504bfb
José Victor <victorlimaf55@gmail.com>
2024-03-02 17:17:30 -0300

    feature: mokando os teste de StockController

--- src/test/java/com/mandacarubroker/controller/StockControllerTest.java
+++ src/test/java/com/mandacarubroker/controller/StockControllerTest.java
@@ -1,74 +1,83 @@
 package com.mandacarubroker.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mandacarubroker.domain.stock.RequestStockDTO;
 import com.mandacarubroker.domain.stock.Stock;
+import com.mandacarubroker.domain.stock.StockRepository;
+import com.mandacarubroker.service.StockService;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
-import java.util.Random;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-@SpringBootTest
-@AutoConfigureMockMvc
+@DataJpaTest
+@ActiveProfiles("test")
 class StockControllerTest {
 
     @Autowired
-    MockMvc mockMvc;
-    @Test
-    void getAllStockRequestStatusOk() throws Exception {
-        this.mockMvc.perform(get("/stocks")).andDo(print()).andExpect(status().isOk());
+    StockRepository stockRepository;
+
+    StockService stockService;
+
+    @Autowired
+    EntityManager entityManager;
+
+    private Stock createStock(RequestStockDTO stockDTO) {
+        Stock newStock = new Stock(stockDTO);
+        this.entityManager.persist(newStock);
+        return newStock;
     }
 
-    
-
-    @Test
-    void getStockById() throws Exception {
-        String id = "4948d1c1-ecd5-4840-bd19-70d955ae2fda";
-        this.mockMvc.perform(get("/stocks/4948d1c1-ecd5-4840-bd19-70d955ae2fda")).andDo(print())
-                .andExpect(status().isOk());
-    }
-    @Test
-    void getStockByIdMostReturnEmptyIfIdNotFound() throws Exception {
-        this.mockMvc.perform(get("/stocks/notId")).andDo(print()).andExpect(status().isOk())
-                .andExpect(content().string(""));
+    private void createService() {
+        this.stockService = new StockService(this.stockRepository);
     }
 
     @Test
-    void createNewStock() throws Exception {
-        RequestStockDTO request = new RequestStockDTO("CP1", "Testing Company", 123);
-        Stock stock = new Stock(request);
-        ObjectMapper objectMapper = new ObjectMapper();
-        String jsonStock = objectMapper.writeValueAsString(stock);
+    @DisplayName("Should get stock successfully if ID exists")
+    void getStockById() {
+        createService();
+        RequestStockDTO request = new RequestStockDTO("AB5", "Company", 12.4);
+        Stock stock = this.createStock(request);
 
-        this.mockMvc.perform(post("/stocks").contentType("application/json").content(jsonStock))
-                .andExpect(status().isOk());
+        Optional<Stock> foundStock = this.stockService.getStockById(stock.getId());
+
+        Assertions.assertTrue(foundStock.isPresent());
     }
 
     @Test
-    void updateStock() throws Exception {
-        String id = "4948d1c1-ecd5-4840-bd19-70d955ae2fda";
-        Random r = new Random();
-        RequestStockDTO request =
-                new RequestStockDTO("AB1", "Testing Company", r.nextDouble() * 100);
-        Stock stock = new Stock(request);
-        ObjectMapper objectMapper = new ObjectMapper();
-        String jsonStock = objectMapper.writeValueAsString(stock);
+    @DisplayName("Shouldn't get stock if ID don't exists")
+    void dontGetStockById() {
+        createService();
+        String id = "not Id";
 
-        this.mockMvc.perform(put("/stocks/" + id).contentType("application/json").content(jsonStock))
-                .andExpect(status().isOk());
+        Optional<Stock> foundStock = this.stockService.getStockById(id);
+
+        Assertions.assertFalse(foundStock.isPresent());
     }
 
     @Test
-    void deleteStock() throws Exception {
-        String id = "4948d1c1-ecd5-4840-bd19-70d955ae2fda";
-        this.mockMvc.perform(delete("/stocks/" + id)).andExpect(status().isOk());
+    @DisplayName("Should get all stocks successfully")
+    void getAllStocks() {
+        createService();
+        RequestStockDTO request1 = new RequestStockDTO("AB5", "Company 1", 12.4);
+        RequestStockDTO request2 = new RequestStockDTO("ca2", "Company 2", 32.4);
+        RequestStockDTO request3 = new RequestStockDTO("CY9", "Company 3", 0.4);
+        List<Stock> stockList = new ArrayList<>();
+        stockList.add(createStock(request1));
+        stockList.add(createStock(request2));
+        stockList.add(createStock(request3));
+
+        List<Stock> foundStocks = this.stockService.getAllStocks();
+
+        Assertions.assertArrayEquals(stockList.toArray(), foundStocks.toArray());
     }
+
+
+
 }
